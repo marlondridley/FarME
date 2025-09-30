8ac03903c65645851090ced66701413800ec77c7
Marlon Ridley <marlon.ridley@gmail.com>
2025-09-30 18:44:37 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Failed t

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth, initializeAuth, indexedDBLocalPersistence, setPersistence } from 'firebase/auth';
+import { getAuth, initializeAuth, indexedDBLocalPersistence } from 'firebase/auth';
 import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
 const firebaseConfig = {
