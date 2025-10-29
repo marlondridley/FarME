81820416015284d2aabbf10b7835701d04c47131
Marlon Ridley <marlon.ridley@gmail.com>
2025-10-29 17:33:09 +0000

    i want to move to github

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     env: {
         NEXT_PUBLIC_USDA_API_KEY: process.env.USDA_API_KEY,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-            }
-        ],
-    },
+    }
 };
 
 export default nextConfig;
